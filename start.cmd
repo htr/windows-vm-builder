3554f3523159afbaa94d4dca66b07390b96e4f98
Hugo Tavares Reis <hugo@linux.com>
2020-09-17 15:44:32 +0000

    initial commit

---
+++ start.cmd
@@ -0,0 +1,16 @@
+wpeinit
+c:
+mkdir c:\mount
+dism /mount-wim /wimfile:c:\boot.wim /index:2 /mountdir:c:\mount
+dism /image:c:\mount /add-driver /driver:c:\drivers /recurse
+dism /unmount-wim /mountdir:c:\mount /commit
+
+dism /mount-wim /wimfile:c:\install.wim /index:6 /mountdir:c:\mount
+dism /image:c:\mount /add-driver /driver:c:\drivers /recurse
+dism /unmount-wim /mountdir:c:\mount /commit
+
+date /T > c:\done
+wpeutil shutdown
+
+
+
