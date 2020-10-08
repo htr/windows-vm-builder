4a0e82d9f4745c1c151f736c19d9def549b178dd
Hugo Tavares Reis <hugo@linux.com>
2020-10-09 01:38:34 +0200

    windows 2016 support

--- start.cmd
+++ start.cmd
@@ -5,7 +5,7 @@ dism /mount-wim /wimfile:c:\boot.wim /index:2 /mountdir:c:\mount
 dism /image:c:\mount /add-driver /driver:c:\drivers /recurse
 dism /unmount-wim /mountdir:c:\mount /commit
 
-dism /mount-wim /wimfile:c:\install.wim /index:6 /mountdir:c:\mount
+dism /mount-wim /wimfile:c:\install.wim /index:2 /mountdir:c:\mount
 dism /image:c:\mount /add-driver /driver:c:\drivers /recurse
 dism /unmount-wim /mountdir:c:\mount /commit
 
